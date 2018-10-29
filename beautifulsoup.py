4a7d4141a0fbe5c23b9db85478450b9800d4b59f
Mathias Bigler <Orchidevej@hotmail.com>
2018-10-29 17:12:31 +0100

    Update beautifulsoup.py

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -6,33 +6,6 @@ import networkx as nx
 start_url = 'http://www.kultunaut.dk/perl/arrlist/type-nynaut/UK?showmap=&Area=Kbh.+og+Frederiksberg&periode=&Genre=Musik'
 start_url2 = 'http://www.pyregex.com/'
 
-all_links = []
-count = 0
-
-# # returns a list of all links
-# def find_all_links_in_links(url, depth): # depth = 2 is preferred - otherwise way too long runtime
-#     try: 
-#         print(depth)
-#         if depth > 0:
-#             #print(len(all_links))
-#             data = requests.get(url)
-#             soup = bs4.BeautifulSoup(data.text, 'html5lib')
-#             links = [a['href'] for a in soup.find_all('a', href=True) if a.text]
-#             if len(links) != 0:
-#                 for link in links:
-#                     if(link.startswith('http') and link not in all_links):
-#                         print(link)
-#                         all_links.append(url)
-#                         find_all_links_in_links(link, depth-1)
-#                     else:
-#                         print('Link already found!')
-#     except:
-#         print('An error has happened..!')
-
-#find_all_links_in_links(start_url2, 3)
-#print(len(all_links))
-
-# dictionary example
 links_dic = {}
 def find_all_links(url, depth): 
         try: 
@@ -60,7 +33,6 @@ def plot_links():
     find_all_links(start_url2, 2)
     print(len(links_dic.keys()))
 
-    # Use Helge reference for creating plt
     plot = []
     for key, links in links_dic.items():
         for link in links:
